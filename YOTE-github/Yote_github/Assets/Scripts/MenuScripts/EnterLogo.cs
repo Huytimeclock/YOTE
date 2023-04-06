0095a22bc3a440872d77b26e7a19c7f42e51a01d
Huytimeclock <Huytimeclockz@gmail.com>
2023-04-06 09:05:35 +0700

    Initial change

--- YOTE-github/Yote_github/Assets/Scripts/MenuScripts/EnterLogo.cs
+++ YOTE-github/Yote_github/Assets/Scripts/MenuScripts/EnterLogo.cs
@@ -8,6 +8,7 @@ public class EnterLogo : MonoBehaviour
     [SerializeField] GameObject Button2;
     [SerializeField] GameObject Button3;
     [SerializeField] GameObject Button4;
+    bool onenter = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +17,24 @@ public class EnterLogo : MonoBehaviour
         Button3.SetActive(false);
         Button4.SetActive(false);
     }
-
+    public bool ReturnOnEnter() // use to check if the bar is already collapsed so u can call in EnterPlaySesson ( will use later )
+    {
+        return onenter;
+    }
     private void OnMouseDown()
     {
-        Button1.SetActive(true);
-        Button2.SetActive(true);
-        Button3.SetActive(true);
-        Button4.SetActive(true);
-        Button1.transform.Translate(-3.4f, 0, 0);
-        Button2.transform.Translate(3.4f, 0, 0);
-        Button3.transform.Translate(-3.4f, 0, 0);
-        Button4.transform.Translate(3.4f, 0, 0);
+        while (onenter==false)  // move bar
+        {
+            Button1.SetActive(true);
+            Button2.SetActive(true);
+            Button3.SetActive(true);
+            Button4.SetActive(true);
+            Button1.transform.Translate(-3.4f, 0, 0);
+            Button2.transform.Translate(3.4f, 0, 0);
+            Button3.transform.Translate(-3.4f, 0, 0);
+            Button4.transform.Translate(3.4f, 0, 0);
+            onenter = true;
+        }
+       
     }
 }
---
+++ YOTE-github/Yote_github/Assets/Scripts/MenuScripts/EnterPlaySesson.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterPlaySesson : MonoBehaviour
+{
+    [SerializeField] GameObject bar1;
+
+    private void Update()
+    {
+        // bool CheckOnEnter = FindObjectOfType<EnterLogo>().ReturnOnEnter();  ---------------- will use later
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+
+}
