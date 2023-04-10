d8ce72023352a9a2c85d0cb08e006e7806d4d5a0
Huytimeclock <Huytimeclockz@gmail.com>
2023-04-10 16:02:01 +0700

    .

--- YOTE-github/Yote_github/Assets/Scripts/ChangeImageColorNormalInput.cs
+++ YOTE-github/Yote_github/Assets/Scripts/ChangeImageColorNormalInput.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChangeImageColor : MonoBehaviour
+public class ChangeImageColorNormalInput : MonoBehaviour
 {
+    [SerializeField] string keytopress;
     public Image targetImage;
     public Color targetColor = Color.red;
-    public float duration = 1.0f;
+    public float duration = 0f;
+    public float returnDuration = 0.1f;
 
     private Color _startColor;
     private bool _isColorChanging = false;
+    private bool _isColorReturning = false;
     private float _colorChangeTime = 0.0f;
+    private float _returnTime = 0.0f;
 
     private void Start()
     {
@@ -20,10 +24,10 @@ public class ChangeImageColor : MonoBehaviour
     private void Update()
     {
         // Check if the "H" key is pressed down
-        if (Input.GetKeyDown(KeyCode.H))
+        if (Input.GetKeyDown(keytopress))
         {
             // Start the color change if it is not already in progress
-            if (!_isColorChanging)
+            if (!_isColorChanging && !_isColorReturning)
             {
                 _isColorChanging = true;
                 _colorChangeTime = 0.0f;
@@ -49,6 +53,30 @@ public class ChangeImageColor : MonoBehaviour
             if (_colorChangeTime >= duration)
             {
                 _isColorChanging = false;
+                _isColorReturning = true;
+                _returnTime = 0.0f;
+            }
+        }
+
+        // Check if the color return is in progress
+        if (_isColorReturning)
+        {
+            // Calculate the normalized time for the color return
+            float t = Mathf.Clamp01(_returnTime / returnDuration);
+
+            // Calculate the new color for the image
+            Color newColor = Color.Lerp(targetColor, _startColor, t);
+
+            // Set the new color for the image
+            targetImage.color = newColor;
+
+            // Update the color return time
+            _returnTime += Time.deltaTime;
+
+            // Check if the color return is complete
+            if (_returnTime >= returnDuration)
+            {
+                _isColorReturning = false;
             }
         }
     }
