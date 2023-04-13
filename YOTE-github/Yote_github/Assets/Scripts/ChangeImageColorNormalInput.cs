9f39b4ce51ac60a1f9bbc2144babccd2349f9b88
Huytimeclock <Huytimeclockz@gmail.com>
2023-04-13 10:19:56 +0700

    Air input

--- YOTE-github/Yote_github/Assets/Scripts/ChangeImageColorNormalInput.cs
+++ YOTE-github/Yote_github/Assets/Scripts/ChangeImageColorNormalInput.cs
@@ -6,14 +6,12 @@ public class ChangeImageColorNormalInput : MonoBehaviour
     [SerializeField] string keytopress;
     public Image targetImage;
     public Color targetColor = Color.red;
-    public float duration = 0f;
-    public float returnDuration = 0.1f;
+    public Color targetAirColor = Color.green;
+    bool isShiftPressed = false;
+
 
     private Color _startColor;
-    private bool _isColorChanging = false;
-    private bool _isColorReturning = false;
-    private float _colorChangeTime = 0.0f;
-    private float _returnTime = 0.0f;
+
 
     private void Start()
     {
@@ -23,61 +21,37 @@ public class ChangeImageColorNormalInput : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isShiftPressed = true;
+        }
+
         // Check if the "H" key is pressed down
-        if (Input.GetKeyDown(keytopress))
+        if (Input.GetKey(keytopress) && isShiftPressed==false)
         {
-            // Start the color change if it is not already in progress
-            if (!_isColorChanging && !_isColorReturning)
-            {
-                _isColorChanging = true;
-                _colorChangeTime = 0.0f;
-            }
-        }
-
-        // Check if the color change is in progress
-        if (_isColorChanging)
-        {
-            // Calculate the normalized time for the color change
-            float t = Mathf.Clamp01(_colorChangeTime / duration);
-
             // Calculate the new color for the image
-            Color newColor = Color.Lerp(_startColor, targetColor, t);
+            Color newColor = targetColor;
 
             // Set the new color for the image
             targetImage.color = newColor;
-
-            // Update the color change time
-            _colorChangeTime += Time.deltaTime;
-
-            // Check if the color change is complete
-            if (_colorChangeTime >= duration)
-            {
-                _isColorChanging = false;
-                _isColorReturning = true;
-                _returnTime = 0.0f;
-            }
         }
-
-        // Check if the color return is in progress
-        if (_isColorReturning)
+       else if (Input.GetKey(keytopress) && isShiftPressed)
         {
-            // Calculate the normalized time for the color return
-            float t = Mathf.Clamp01(_returnTime / returnDuration);
 
-            // Calculate the new color for the image
-            Color newColor = Color.Lerp(targetColor, _startColor, t);
-
+            Color newColor = targetAirColor;
+            Debug.Log("activated!");
             // Set the new color for the image
             targetImage.color = newColor;
 
-            // Update the color return time
-            _returnTime += Time.deltaTime;
-
-            // Check if the color return is complete
-            if (_returnTime >= returnDuration)
-            {
-                _isColorReturning = false;
-            }
         }
+
+        else
+        {
+            targetImage.color = _startColor;
+            isShiftPressed=false;
+        }
+
+
+
     }
 }
