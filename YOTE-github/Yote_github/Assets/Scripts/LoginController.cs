a9b9ed301058b5cfc0718d6418784297bad88031
Huytimeclock <Huytimeclockz@gmail.com>
2023-06-27 17:25:58 +0700

    logic code for button

--- YOTE-github/Yote_github/Assets/Scripts/LoginController.cs
+++ YOTE-github/Yote_github/Assets/Scripts/LoginController.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LoginController : MonoBehaviour
 {
     public GameObject LoginSection;
     public GameObject RegisterSection;
+    public GameObject ErrorMessage;
+    private bool isActive = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         LoginSection.SetActive(false);
         RegisterSection.SetActive(false);
+        ErrorMessage.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,7 +27,31 @@ public class LoginController : MonoBehaviour
 
     public void Back_to_login_from_regis()
     {
+        LoginSection.SetActive(true);
+        RegisterSection.SetActive(false);
+    }
+
+    public void Go_to_regis_from_login()
+    {
         LoginSection.SetActive(false);
         RegisterSection.SetActive(true);
     }
+
+    public void AppearOrDisappearLoginSection()
+    {
+        if (isActive)
+        {
+            LoginSection.SetActive(false);
+            RegisterSection.SetActive(false);
+            isActive = false;
+            
+        }
+        else
+        {
+            LoginSection.SetActive(true);
+            isActive = true;
+            
+        }
+    }
+
 }
